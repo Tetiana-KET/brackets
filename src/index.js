09aef91c684f6e3637f8461521cd4a5387e9a89a
Tetiana-KET <belangelphone@gmail.com>
2022-08-11 16:41:31 +0300

    refactor: change solution

--- src/index.js
+++ src/index.js
@@ -1,38 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
 
-    const OPEN_BRACKETS = ['(', '{', '[','|'];
-    const BRACKETS_PAIR = {};
-    // [')']: '(',
-    // ['}']: '{',
-    // [']']: '[',
-    //'()' - строка
-    bracketsConfig.forEach(element => {
-        BRACKETS_PAIR[`${element[1]}`] = `${element[0]}`;
-    })
+  const BRACKETS_PAIR = {};
 
-    let stack = [];
+  bracketsConfig.forEach(element => {
+    BRACKETS_PAIR[`${element[1]}`] = `${element[0]}`;
+  });
 
-    for (let i = 0; i < str.length; i++) {
-        let currentSymbol = str[i];
+  let stack = [];
 
-        if (OPEN_BRACKETS.includes(currentSymbol)) {//Проверяем или текущий символ - открывающая скобка
-            stack.push(currentSymbol);// если да - кладем ее в стек
-        } else {// если НЕТ, не открывающая - значит закрывающая
-            if (stack.length === 0) { //то проверяем или стек пуст, если стек пуст, а текущая скобка закрывает,
-                return false;// значит порядок нарушен, в стеке нет открывающей скобки -  и возвращаем фалс
-            }
+  for (let i = 0; i < str.length; i++) {
 
-            let topElement = stack[stack.length - 1];// если стек не пуст, проверяем верхний элемент
+    let currentSymbol = str[i];
 
-            //проверяем совпадает ли элемент в стеке паре для текущего символа
-            if (BRACKETS_PAIR[currentSymbol] === topElement) {//на данном этапе текущий символ - закрывающий [')']: '(', а в стеке у нас уже лежит '('
-                stack.pop();
-            }
-            else {//если скобка, которая пришла и которая лежит в стеке отличается, то фалс
-                return false;
-            }
-        }
+    let topElement = stack[stack.length - 1];
+
+    if (stack.length > 0 && topElement === BRACKETS_PAIR[currentSymbol]) {
+      stack.pop();
+    } else {
+      stack.push(currentSymbol);
     }
-    return stack.length === 0;// если после всех проверок стек пуст - то последовательность верна
+  }
+  
+  return stack.length === 0;
 
 }
